982a75d58b4e1207be6a2dda50f1463bfd3fbf35
Aditi Pisurde <168277373+aditipisurde17@users.noreply.github.com>
2024-09-18 23:10:41 +0530

    Add files via upload

---
+++ sum_type1.cpp
@@ -0,0 +1,20 @@
+// in parameterized sum we usually add numbers directly to parameters
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int i , int sum){
+    if (i<1) {
+        cout<<sum;
+         return ;
+    }
+
+    print(i-1,sum+i);
+}
+    
+
+int main(){
+    int n;
+    cin>>n;
+    print(n,0);
+}
